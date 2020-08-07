d06ec030f8b125b799a31d99faca1dec928c58b9
Sarah Shealy <sshealy124@gmail.com>
2020-08-06 20:11:52 -0400

    added to members.js

--- public/js/members.js
+++ public/js/members.js
@@ -4,4 +4,14 @@ $(document).ready(() => {
   $.get("/api/user_data").then(data => {
     $(".member-name").text(data.email);
   });
+
+$(".nav-link").on("click", function(e){
+  e.preventDefault();
+
+  console.log($(this).text());
+  console.log(this); 
+  $("#monthTitle").text($(this).text());
+  $(".card").removeClass("hide");
 });
+});
+
