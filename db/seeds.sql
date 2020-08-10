089d81c090bc651da9d2d0319ef2e0daba5ce294
Laura Lara <llaraama@uncc.edu>
2020-08-09 21:33:56 -0400

    added a navbar and changed the css to make it responisve, add a new background

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO birthdays (firstname,lastname,date,gift) VALUES ('Sarah','Shealy'.'1997-12-04','purse');
+INSERT INTO birthdays (firstname,lastname,date,gift) VALUES ('Sarah','Shealy','1997-12-04','purse');
 INSERT INTO birthdays (firstname,lastname,date,gift) VALUES ('Laura','Lara','1995-02-23','tv');
 
 
